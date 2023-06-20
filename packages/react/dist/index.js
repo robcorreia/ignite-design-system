1c8ec7c9e1253ed35041eb59ac0b05d96331f4bc
Robson Correia <robsoncorreia@192.168.100.3>
2023-06-20 17:25:58 -0300

    feat: add turbo repo

--- packages/react/dist/index.js
+++ packages/react/dist/index.js
@@ -144,8 +144,26 @@ var {
 // src/index.tsx
 var Button = styled("button", {
   fontFamily: "$default",
-  backgroundColor: "$ignite500",
-  borderRadius: "$md"
+  backgroundColor: "$ignite300",
+  borderRadius: "$sm",
+  border: 0,
+  fontWeight: "bold",
+  color: "$white",
+  variants: {
+    variant: {
+      small: {
+        fontSize: 14,
+        padding: "$2 $4"
+      },
+      big: {
+        fontSize: 16,
+        padding: "$3 $6"
+      }
+    }
+  },
+  defaultVariants: {
+    variant: "small"
+  }
 });
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
