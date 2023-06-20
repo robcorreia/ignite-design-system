d24141d7f77c197ec1884e4feac25f29d38e1099
Robson Correia <robsoncorreia@192.168.100.3>
2023-06-20 13:16:43 -0300

    feat: add tokens and colors docs in storybook

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,7 +1,21 @@
 import type { StoryObj, Meta } from '@storybook/react'
-import { Button } from '@ignite-ui/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
 
 export default {
-  title: "Button",
+  title: 'Form/Button',
   component: Button,
-} as Meta
+
+  args: {
+    children: 'Enviar'
+  }
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {
+
+}
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'big'
+  }
+}
